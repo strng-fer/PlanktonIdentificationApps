1d7c8266598afd80acca09eedc7068ac32b86817
strng-fer <feryadiyulius24@gmail.com>
2025-07-01 09:05:53 +0700

    testing

---
+++ app/src/main/java/com/example/planktondetectionapps/MainActivity.kt
@@ -0,0 +1,120 @@
+package com.example.planktondetectionapps
+
+import com.example.planktondetectionapps.R
+import android.view.View
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.media.ThumbnailUtils
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.planktondetectionapps.ml.ModelUnquant
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.io.IOException
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import kotlin.math.min
+
+class MainActivity : AppCompatActivity() {
+    var result: TextView? = null
+    var confidence: TextView? = null
+    var imageView: ImageView? = null
+    var picture: Button? = null
+    var imageSize: Int = 224
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        result = findViewById<TextView>(R.id.result)
+        confidence = findViewById<TextView>(R.id.confidence)
+        imageView = findViewById<ImageView>(R.id.imageView)
+        picture = findViewById<Button>(R.id.button)
+
+        picture?.setOnClickListener(View.OnClickListener {
+            // Launch camera if we have permission
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(cameraIntent, 1)
+            } else {
+                //Request camera permission if we don't have it.
+                requestPermissions(arrayOf(Manifest.permission.CAMERA), 100)
+            }
+        })
+    }
+
+    fun classifyImage(image: Bitmap) {
+        try {
+            val model: ModelUnquant = ModelUnquant.newInstance(applicationContext)
+
+            // Creates inputs for reference.
+            val inputFeature0 =
+                TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.FLOAT32)
+            val byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3)
+            byteBuffer.order(ByteOrder.nativeOrder())
+
+            val intValues = IntArray(imageSize * imageSize)
+            image.getPixels(intValues, 0, image.width, 0, 0, image.width, image.height)
+            var pixel = 0
+            for (i in 0..<imageSize) {
+                for (j in 0..<imageSize) {
+                    val `val` = intValues[pixel++] // RGB
+                    byteBuffer.putFloat(((`val` shr 16) and 0xFF) * (1f / 255f))
+                    byteBuffer.putFloat(((`val` shr 8) and 0xFF) * (1f / 255f))
+                    byteBuffer.putFloat((`val` and 0xFF) * (1f / 255f))
+                }
+            }
+
+            inputFeature0.loadBuffer(byteBuffer)
+
+            // Runs model inference and gets result.
+            val outputs: ModelUnquant.Outputs = model.process(inputFeature0)
+            val outputFeature0: TensorBuffer = outputs.getOutputFeature0AsTensorBuffer()
+
+            val confidences = outputFeature0.floatArray
+            var maxPos = 0
+            var maxConfidence = 0f
+            for (i in confidences.indices) {
+                if (confidences[i] > maxConfidence) {
+                    maxConfidence = confidences[i]
+                    maxPos = i
+                }
+            }
+
+            val classes = arrayOf("Table", "Cupboard")
+
+            result!!.text = classes[maxPos]
+
+            var s = ""
+            for (i in classes.indices) {
+                s += String.format("%s: %.1f%%\n", classes[i], confidences[i] * 100)
+            }
+
+            confidence!!.text = s
+
+            // Releases model resources if no longer used.
+            model.close()
+        } catch (e: IOException) {
+            // TODO Handle the exception
+        }
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            var image = data!!.extras!!["data"] as Bitmap?
+            val dimension = min(image!!.width, image.height)
+            image = ThumbnailUtils.extractThumbnail(image, dimension, dimension)
+            imageView!!.setImageBitmap(image)
+
+            image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false)
+            classifyImage(image)
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+}
